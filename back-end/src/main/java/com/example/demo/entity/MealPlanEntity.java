1e1dbce38bbfed215e8367c94d2328c9e6daec59
Migara Vimukthi <99311565+it21295638@users.noreply.github.com>
2024-06-03 04:07:32 +0530

    backend

---
+++ back-end/src/main/java/com/example/demo/entity/MealPlanEntity.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "meal_planes")
+public class MealPlanEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String mealName;
+    @Lob
+    private Blob post;
+    private String description;
+    private String recipe;
+    private String nutrition;
+    @Column(name = "`portion`") // Escape the reserved keyword
+    private String portion;
+    private String schedule;
+    // Dietary preferences
+    private boolean vegetarian;
+    private boolean vegan;
+    private boolean glutenFree;
+    private boolean dairyFree;
+    private boolean nutFree;
+
+}
